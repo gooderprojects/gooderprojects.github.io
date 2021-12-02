8ffa3ef59d0f218ba972b1b76261c9c9d686a332
Lucas <hxl5900@gmail.com>
2021-12-02 12:16:14 -0500

    added main files

---
+++ main.js
@@ -0,0 +1,24 @@
+var myfunc = setInterval(function() {
+  var now = new Date().getTime();
+  // This gets the actual date
+  document.getElementById("current_date").innerHTML = new Date();
+  // This is for the end of the school year
+  var countDownDate = new Date("May 31, 2022 15:00:00").getTime();
+
+  var timeleft = countDownDate - now;
+
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  document.getElementById("end_days").innerHTML = days
+  document.getElementById("end_hours").innerHTML = hours
+  document.getElementById("end_minutes").innerHTML = minutes
+  document.getElementById("end_seconds").innerHTML = seconds
+
+  if (timeleft < 0) {
+      clearInterval(myfunc);
+      console.log("End");
+  }
+}, 1000)
