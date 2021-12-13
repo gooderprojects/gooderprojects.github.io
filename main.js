7f9018d0d8fed1da40f3e5d466e77a2fac05c3d9
Lucas <hxl5900@gmail.com>
2021-12-13 09:19:42 -0500

    Added spring break timer

--- main.js
+++ main.js
@@ -55,6 +55,35 @@ function christmasbTimer(countDownDate, now) {
   }
 }
 
+function springbTimer(countDownDate, now) {
+  var timeleft_end = countDownDate - now;
+
+  var days = Math.floor(timeleft_end / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft_end % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft_end % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft_end % (1000 * 60)) / 1000);
+
+  document.getElementById("sbreak_days").innerHTML = days;
+  document.getElementById("sbreak_hours").innerHTML = hours;
+  document.getElementById("sbreak_minutes").innerHTML = minutes;
+  document.getElementById("sbreak_seconds").innerHTML = seconds;
+
+  if (days <= 7) {
+    document.getElementById("sbreak_span").style.color = "red";
+    document.getElementById("sbreak_days").style.color = "red";
+    document.getElementById("sbreak_hours").style.color = "red";
+    document.getElementById("sbreak_minutes").style.color = "red";
+    document.getElementById("sbreak_seconds").style.color = "red";
+    document.getElementById("sbreak_subtitle").style.color = "red";
+  }
+
+  if (timeleft_end < 0) {
+      clearInterval(myfunc);
+      console.log("End");
+      window.open("cbreak.html")
+  }
+}
+
 var myfunc = setInterval(function() {
   var current_time = new Date().getTime();
   // This gets the actual date
@@ -67,6 +96,10 @@ var myfunc = setInterval(function() {
   // This is for Christmas break
   var countDownDate_cbreak = new Date("Dec 17, 2021 15:00:01").getTime();
   christmasbTimer(countDownDate_cbreak, current_time);
+
+  // This is for spring break
+  var countDownDate_sbreak = new Date("Apr 15, 2022 15:00:01").getTime();
+  springbTimer(countDownDate_sbreak, current_time);
 
 
 }, 1000)
