119a045a219408c470247f2d48597ef463f5d1d0
Lucas <hxl5900@gmail.com>
2021-12-13 09:52:46 -0500

    Added comments to files

--- main.js
+++ main.js
@@ -1,44 +1,56 @@
 function swichToCbreak() {
+  // This opens the timer for the end of Christmas break
   window.open("cbreak.html")
 }
 
+// This is the function for the end of school year timer
 function endTimer(countDownDate, now) {
+  // Gets the time left until the end of the school year
   var timeleft_end = countDownDate - now;
 
+  // Does some calculating stuff
   var days = Math.floor(timeleft_end / (1000 * 60 * 60 * 24));
   var hours = Math.floor((timeleft_end % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeleft_end % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((timeleft_end % (1000 * 60)) / 1000);
 
+  // Changes the numbers on the timer
   document.getElementById("end_days").innerHTML = days;
   document.getElementById("end_hours").innerHTML = hours;
   document.getElementById("end_minutes").innerHTML = minutes;
   document.getElementById("end_seconds").innerHTML = seconds;
 
+  // It it is within 7 days, the timer turns red
   if (days <= 7) {
     document.getElementById("end_span").style.color = "red";
     document.getElementById("end_subtitle").style.color = "red";
   }
 
+  // Does something after the timer ends
   if (timeleft_end < 0) {
       clearInterval(myfunc);
       console.log("End");
   }
 }
 
+// This is the function for the Christmas break timer
 function christmasbTimer(countDownDate, now) {
+  // Gets time until the end of the school year
   var timeleft_end = countDownDate - now;
 
+  // Does some calculating stuff
   var days = Math.floor(timeleft_end / (1000 * 60 * 60 * 24));
   var hours = Math.floor((timeleft_end % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeleft_end % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((timeleft_end % (1000 * 60)) / 1000);
 
+  // Changes the numbers on the timer
   document.getElementById("cbreak_days").innerHTML = days;
   document.getElementById("cbreak_hours").innerHTML = hours;
   document.getElementById("cbreak_minutes").innerHTML = minutes;
   document.getElementById("cbreak_seconds").innerHTML = seconds;
 
+  // It it is within 7 days, the timer turns red
   if (days <= 7) {
     document.getElementById("cbreak_span").style.color = "red";
     document.getElementById("cbreak_days").style.color = "red";
@@ -48,6 +60,7 @@ function christmasbTimer(countDownDate, now) {
     document.getElementById("cbreak_subtitle").style.color = "red";
   }
 
+  // Does something after the timer ends
   if (timeleft_end < 0) {
       clearInterval(myfunc);
       console.log("End");
@@ -55,19 +68,24 @@ function christmasbTimer(countDownDate, now) {
   }
 }
 
+// This is the function for the spring break timer
 function springbTimer(countDownDate, now) {
+  // Gets the time left until spring break
   var timeleft_end = countDownDate - now;
 
+  // Does some calculating stuff
   var days = Math.floor(timeleft_end / (1000 * 60 * 60 * 24));
   var hours = Math.floor((timeleft_end % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeleft_end % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((timeleft_end % (1000 * 60)) / 1000);
 
+  // Changes the number on the timer
   document.getElementById("sbreak_days").innerHTML = days;
   document.getElementById("sbreak_hours").innerHTML = hours;
   document.getElementById("sbreak_minutes").innerHTML = minutes;
   document.getElementById("sbreak_seconds").innerHTML = seconds;
 
+  // Makes the timer red if it is within 7 days
   if (days <= 7) {
     document.getElementById("sbreak_span").style.color = "red";
     document.getElementById("sbreak_days").style.color = "red";
@@ -77,6 +95,7 @@ function springbTimer(countDownDate, now) {
     document.getElementById("sbreak_subtitle").style.color = "red";
   }
 
+  // Ends the timer
   if (timeleft_end < 0) {
       clearInterval(myfunc);
       console.log("End");
@@ -84,8 +103,11 @@ function springbTimer(countDownDate, now) {
   }
 }
 
+// This is the main function
 var myfunc = setInterval(function() {
+  // Gets the current time
   var current_time = new Date().getTime();
+  
   // This gets the actual date
   document.getElementById("current_date").innerHTML = new Date();
 
