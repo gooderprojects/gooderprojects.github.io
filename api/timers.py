f420cc01cb46364c211547e5a25fa875d061fcd5
ZBOMB <hxl9500@gmail.com>
2022-05-03 14:21:36 -0400

    Actually started working on api script

--- api/timers.py
+++ api/timers.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 
 from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
-import pandas as pd
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from pyvirtualdisplay import Display
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+def getSeconds():
+    display = Display(visible=0, size=(800, 600))
+    display.start()
 
-products=[] # List to store name of the product
-prices=[] # List to store price of the product
-ratings=[] # List to store rating of the product
-driver.get("<a href="https://www.flipkart.com/laptops/">https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&amp;amp;amp;amp;amp;amp;amp;amp;amp;uniq")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://gooderprojects.github.io/school/json/index.html")
 
-content = driver.page_source
-soup = BeautifulSoup(content)
-for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
-name=a.find('div', attrs={'class':'_3wU53n'})
-price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
-rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
-products.append(name.text)
-prices.append(price.text)
-ratings.append(rating.text)
+    data = driver.find_element(By.ID, "end-seconds").text
 
-df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings})
-df.to_csv('products.csv', index=False, encoding='utf-8')
+    driver.quit()
+    display.stop()
+
+    return data
