55c4c3c02df800ac1e934bf7b24226467bdc8d86
ZBOMB <hxl9500@gmail.com>
2022-05-04 14:15:31 -0400

    Finished example.py

--- api/example.py
+++ api/example.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
 
-# This is a demo script for getting timer information from the website
-# The following code displays how many seconds are currently on the 'end timer'
+# This is a demo script for getting timer information from the website usring the api
+# The following code displays how much time is currently left on the 'end timer'
 
-# These are important
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from pyvirtualdisplay import Display
-import time
+from timers import Timers
 
-# Hides chrome window
-display = Display(visible=0, size=(800, 600))
-display.start()
+timers = Timers()
 
-# Connects to the website
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://gooderprojects.github.io/school/json/index.html")
-data = 0
-
-# Constantly gets data from website
-while True:
-    if driver.find_element(By.ID, "end-seconds").text != data:
-        data = driver.find_element(By.ID, "end-seconds").text # Gets data
-        print(data) # Outputs data
-    time.sleep(0.1) # Increase if you want better cpu performance, but less data accuraccy. Decrease if you hate cpu but want realtime data.
+print()
+print("Time Until the End of the School Year:")
+print(timers.getDays("end")+":"+timers.getHours("end")+":"+timers.getMinutes("end")+":"+timers.getSeconds("end"))
+print()
