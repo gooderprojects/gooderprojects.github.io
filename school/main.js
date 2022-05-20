cb73834143421a61039d4a01e83ccc36f31735a8
zbom <hxl9500@gmail.com>
2022-05-20 11:36:04 -0400

    Fixed maintenance page

--- school/main.js
+++ school/main.js
@@ -36,9 +36,10 @@ function staticTimer(timer, countDownDate, now, static_timer) {
     }
   }
 
-  if (timeleft_end < 0) {
-    window.open("maintenance.html", '_self');
+  if (timeleft_end <= 0) {
     console.log("End");
+    clearInterval(myfunc);
+    window.open("maintenance.html", '_self');
   }
 }
 
@@ -56,7 +57,7 @@ var myfunc = setInterval(function() {
   var countDownDate_end = new Date("May 26, 2022 15:00:01").getTime();
   staticTimer("end", countDownDate_end, current_time, true);
 
-  var countDownDate_x = new Date("May 25, 2022 15:00:01").getTime();
+  var countDownDate_x = new Date("May 19, 2022 15:00:01").getTime();
   staticTimer("x", countDownDate_x, current_time, true);
 
   // No delay on open
