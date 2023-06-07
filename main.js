ef1c8c19be63161cf2e1849f24927d5d7230b159
ZBOMB <hxl9500@gmail.com>
2023-06-07 12:11:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ var myfunc = setInterval(function() {
   document.getElementById("current-date").innerHTML = new Date();
 
   // Runs all timers
-  var countDownDate_end = new Date("May 22, 2023 15:00:01").getTime();
+  var countDownDate_end = new Date("Aug 12, 2036 00:00:01").getTime();
   endTimer(countDownDate_end, current_time);
 
   // No delay on open
