7a8bccdca355223f616a5127fdd3e82e68623bd8
ZBOMB <hxl9500@gmail.com>
2024-04-17 09:23:30 -0400

    Update random_header.js

--- ixhell/IXHell-beta/js/random_header.js
+++ ixhell/IXHell-beta/js/random_header.js
@@ -1,28 +1,22 @@
 function randomizeHeader() {
-  var random_var = Math.floor(Math.random() * 13);
+  var random_var = Math.floor(Math.random() * 9);
   if (random_var == 1) {
     return "You're an idiot...";
   } else if (random_var == 0) {
     return "This is why you have no friends...";
-  } else if (random_var == 2) {
-    return "Get smarter...";
-  } else if (random_var == 3) {
+  } else if (random_var == 1) {
     return "Uh oh, retard alert.";
-  } else if (random_var == 4) {
+  } else if (random_var == 2) {
     return "Nobody likes you...";
-  } else if (random_var == 5) {
-    return "I could sense your stupidity from here";
-  } else if (random_var == 6) {
+  } else if (random_var == 3) {
     return "you dumb kys";
-  } else if (random_var == 10) {
+  } else if (random_var == 4) {
     return "this is why you get zero b*tches";
-  } else if (random_var == 7) {
+  } else if (random_var == 5) {
     return "i can smell you";
-  } else if (random_var == 8) {
-    return "have you considered IXL isn't right for you? ";
-  } else if (random_var == 11) {
+  } else if (random_var == 6) {
     return "Yo mama so poor, the ducks throw bread at her";
-  } else if (random_var == 9) {
+  } else if (random_var == 7) {
     return "Yo mama so stupid, I saw her yelling into an envelope. When I asked her what she was doing, she said she was leaving a voicemail.";
   } else {
     return "This is why your parents don't love you...";
